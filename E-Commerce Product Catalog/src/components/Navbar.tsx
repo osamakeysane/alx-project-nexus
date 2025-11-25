27c215341dcf27884c776376d48876eb70652a9b
mohamed keytsane <osamakeysane20@gmail.com>
2025-11-25 10:53:42 +0300

    Add navigation links to Navbar component

--- E-Commerce Product Catalog/src/components/Navbar.tsx
+++ E-Commerce Product Catalog/src/components/Navbar.tsx
@@ -29,6 +29,20 @@ const Navbar = () => {
           </button>
         </div>
       </div>
+      <div className="flex items-center justify-center space-x-10 py-4 text-sm font-bold">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <Link to="/" className="hover:underline">
+          Shop
+        </Link>
+        <Link to="/" className="hover:underline">
+          Contact
+        </Link>
+        <Link to="/" className="hover:underline">
+          About
+        </Link>
+      </div>
     </nav>
   );
 };
