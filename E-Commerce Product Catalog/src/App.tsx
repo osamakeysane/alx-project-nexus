dc8463a04a71a8443e7761d9b39959f514aa80f4
mohamed keytsane <osamakeysane20@gmail.com>
2025-11-25 08:54:50 +0300

    Setup routing and add Navbar & Footer layout.

--- E-Commerce Product Catalog/src/App.tsx
+++ E-Commerce Product Catalog/src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route></Route>45
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
