65029d37c1639bad54559ebebc684ad5db74fd7d
mohamed keytsane <osamakeysane20@gmail.com>
2025-11-30 11:50:16 +0300

    Use Redux state `filteredData` to display filtered products - Display products using `ProductCard` in a responsive grid - Show a "not found" image if no products match the search - Ensure proper TypeScript typing for the component

--- E-Commerce Product Catalog/src/pages/FilterData.tsx
+++ E-Commerce Product Catalog/src/pages/FilterData.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import type { RootState } from "../redux/store";
 import ProductCard from "../components/ProductCard";
+import notFound from "../assets/images/not-found.jpeg";
 
 const FilterData: React.FC = () => {
   const filteredProducts = useSelector(
@@ -10,16 +11,20 @@ const FilterData: React.FC = () => {
 
   return (
     <div className="mx-auto py-12 px-4 md:px-16 lg:px-24">
-      <h2 className="text-2xl font-bold mb-6 text-center">Shop</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 cursor-pointer">
-        {filteredProducts.length > 0 ? (
-          filteredProducts.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))
-        ) : (
-          <p className="text-center col-span-full">No products available</p>
-        )}
-      </div>
+      {filteredProducts.length > 0 ? (
+        <>
+          <h2 className="text-2xl font-bold mb-6 text-center">Shop</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 cursor-pointer">
+            {filteredProducts.map((product) => (
+              <ProductCard product={product} />
+            ))}
+          </div>
+        </>
+      ) : (
+        <div className="flex justify-center">
+          <img src={notFound} alt="" />
+        </div>
+      )}
     </div>
   );
 };
